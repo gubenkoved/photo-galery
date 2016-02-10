edc3c72243a6283e8e64a9355d6dcf770615600a
Eugene Gubenkov <gubenkoved@gmail.com>
2016-02-10 23:26:25 +0300

    Basic UI app structure created.

---
+++ PhotosGalery2.Client/app/app.js
@@ -0,0 +1,14 @@
+﻿var settings =
+{
+    'appRoot': 'http://localhost:55196/api'
+};
+
+function AlbumsService() {
+    this.getRoot() = function () {
+        return 'Hello there ' + name;
+    };
+}
+
+angular
+  .module('app')
+  .service('AlbumsService', AlbumsService);
