fb08cd218176db0baba5aa0c063a1d709725744b
Eugene Gubenkov <gubenkoved@gmail.com>
2016-02-10 23:55:49 +0300

    Getting AngularJS autocomplete!

---
+++ PhotosGalery2.Client/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app/app.js" />
+/// <reference path="app/controllers/AlbumsController.js" />
+/// <reference path="bootstrap/js/bootstrap.js" />
+/// <reference path="bootstrap/js/npm.js" />
+/// <reference path="ng/angular.js" />
