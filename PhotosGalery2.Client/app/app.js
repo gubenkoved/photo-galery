c9465ec2d0ab3d9eda965ef3356502b2fd57c300
Eugene Gubenkov <gubenkoved@gmail.com>
2016-02-13 22:05:26 +0300

    Show list of all albums on JS app startup!

--- PhotosGalery2.Client/app/app.js
+++ PhotosGalery2.Client/app/app.js
@@ -1,13 +1,38 @@
-﻿var settings =
-{
-    'appRoot': 'http://localhost:55196/api'
-};
+﻿var app = angular.module('app', []);
 
-angular.module('app', [])
-  .service('AlbumsService', ["$http", function ($http)
+app.constant('config',
+    {
+        apiRoot: 'http://localhost:55196/api',
+        ver: '1.0.0.0'
+    });
+
+app.service('AlbumsService', ["$http", "config", function ($http, config)
   {
-      
-  }]);
+    var service = {};
+
+    service.getRootItems = function ()
+    {
+        console.log('test');
+
+        return $http.get(config.apiRoot + '/albums')
+            .then(function (response) {
+                return response.data;
+            });
+    }
+
+    return service;
+}]);
+
+app.controller('AlbumsController', ['$scope', 'AlbumsService', function ($scope, AlbumsService)
+    {
+        $scope.getRootItems = function ()
+        {
+            return AlbumsService.getRootItems().then(function (items)
+            {
+                $scope.albumItems = items.AlbumItems;
+            });
+        }
+    }]);
 
 /* APP RUN */
 
